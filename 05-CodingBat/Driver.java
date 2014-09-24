6259e75f36764b2c3c93e43f4ea01a154da3a81f
daltonwu <dalton.z.wu@gmail.com>
2014-09-23 20:32:37 -0400

    Okay, for some reason, I can't call the StringStuff methods.

---
+++ 05-CodingBat/Driver.java
@@ -0,0 +1,36 @@
+public class Driver {
+    public static void main(String[] args) {
+        // test everything
+        StringStuff foo = new StringStuff();
+
+        System.out.println("nonstart\n");
+        System.out.print("Expecting \"ellohere\": ");
+        System.out.println(foo.nonStart("Hello", "There"));
+        System.out.print("Expecting \"avaode\": ");
+        System.out.println(foo.nonStart("java", "code"));
+        System.out.print("Expecting \"hotlava\": ");
+        System.out.println(foo.nonStart("shotl", "java"));
+        System.out.print("\n\n");
+        
+        
+        System.out.println("makeAbba\n");
+        System.out.print("Expecting \"HiByeByeHi\": ");
+        System.out.println(foo.makeAbba("Hi", "Bye"));
+        System.out.print("Expecting \"YoAliceAliceYo\": ");
+        System.out.println(foo.makeAbba("Yo", "Alice"));
+        System.out.print("Expecting \"WhatUpUpWhat\": ");
+        System.out.println(foo.makeAbba("What", "Up"));
+        System.out.print("\n\n");
+        
+        System.out.println("diff21\n");
+        System.out.print("Expecting 2: ");
+        System.out.println(foo.diff21(19));
+        System.out.print("Expecting 11: ");
+        System.out.println(foo.diff21(10));
+        System.out.print("Expecting 0: ");
+        System.out.println(foo.diff21(10));
+
+
+        
+    }
+}
---
+++ 05-CodingBat/StringStuff.java
@@ -0,0 +1,12 @@
+public class StringStuff {
+    public String nonstart(String a, String b) {
+        return a.substring(1) + b.substring(1);
+    }
+    public String makeAbba(String a, String b) {
+        return a + b + b + a;
+    }
+    public int diff21(int n) {
+        int diff = Math.abs(n - 21);
+        return n > 21 ? diff * 2 : diff;
+    }
+}
