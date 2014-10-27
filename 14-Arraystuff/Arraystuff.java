3cf3980e716dedb15a46613a9bd6406ab5988ddf
daltonwu <dalton.z.wu@gmail.com>
2014-10-27 09:27:01 -0400

    weofij

---
+++ 14-Arraystuff/Arraystuff.java
@@ -0,0 +1,25 @@
+public class Arraystuff {
+    int[] arr;
+    
+    public Arraystuff(int[] arr) {
+	this.arr = arr;
+    }
+    
+    public int find(n) {
+	/** @return index of first occurrence of n in the array or -1 if not found
+	 *  @param int[], number n
+	 */
+	for(int aye=0; aye<arr.length; aye++) {
+	    if(arr[aye]==n) return aye;
+	}
+	return -1;
+    }
+    public int maxVal() {
+	/** @return largest value in the array
+	 */
+	if(arr.length == 0) return n
+	for(foo : arr) {
+	    if (foo > max) max = foo;
+	}
+	if
+    }
