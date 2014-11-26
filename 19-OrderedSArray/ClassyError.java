8a8a49f338da06e236232be964822dff1a4031f4
daltonwu <dalton.z.wu@gmail.com>
2014-11-26 09:27:37 -0500

    AMgit add OrderedSArray.java !

---
+++ 19-OrderedSArray/ClassyError.java
@@ -0,0 +1,6 @@
+public class ClassyError extends Exception {
+    public ClassyError() {super();}
+    public ClassyError(String message) {super(message);}
+    public ClassyError(String message, Throwable cause) {super(message, cause);}
+    public ClassyError(Throwable cause) {super(cause);}
+}
---
+++ 19-OrderedSArray/Driver.java
@@ -0,0 +1,5 @@
+import java.util.Arrays;
+
+public class Driver {
+    OrderedSArray<String> osa = new OrderedSArray<String>(10, 10);
+}
