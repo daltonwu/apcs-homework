195d1a1ec183c523b1088e4eda25cdd3289948c3
daltonwu <dalton.z.wu@gmail.com>
2014-12-03 22:55:53 -0500

    M

---
+++ 20-sorting1/SortedSuperArray.java
@@ -0,0 +1,13 @@
+public class SortedSuperArray<E extends Comparable<E>> extends SuperArray {
+    public SortedSuperArray(int size, int growSize) {
+        super(size, growSize);
+    }
+    public SortedSuperArray() {
+        super(10, 10);
+    }
+
+    public void insertionSort() {
+            
+    }
+
+}
