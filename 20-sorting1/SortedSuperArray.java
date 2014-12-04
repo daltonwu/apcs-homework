1cdedb7600db51e95a00aa58741ef078fafc6300
daltonwu <dalton.z.wu@gmail.com>
2014-12-04 15:35:51 -0500

    Update SortedSuperArray.java

--- 20-sorting1/SortedSuperArray.java
+++ 20-sorting1/SortedSuperArray.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class SortedSuperArray<E extends Comparable<E>> extends SuperArray {
     public SortedSuperArray(int size, int growSize) {
         super(size, growSize);
@@ -5,7 +7,11 @@ public class SortedSuperArray<E extends Comparable<E>> extends SuperArray {
     public SortedSuperArray() {
         super(10, 10);
     }
-
+    public void chopItUp() {
+        Random rand = new Random();
+        
+    }
+    }
     public void insertionSort() {
         for(int i=0; i<super.size(); i++) {
         }
