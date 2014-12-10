4c3932c50422007c5b5f7acb2ad5354f1c47bdcd
daltonwu <dalton.z.wu@gmail.com>
2014-12-10 18:22:18 -0500

    AM

--- 21-intervals/Foo.java
+++ 21-intervals/Foo.java
@@ -1,8 +1,12 @@
 public class Foo {
     public static void main(String[] args) {
-        Ninja b = new Ninja("Hola", 99, 99);
-        Ninja boo = new Ninja("Cthulu", 99, 99);
-        System.out.println(b.compareTo(boo));
-        System.out.println(boo.compareTo(b));
+        //Basechar b = new Basechar("Hola", 99, 99);
+        //Basechar boo = new Basechar("Cthulu", 99, 99);
+        Ninja n = new Ninja("Halloa");
+        Ninja nuuuu = new Ninja("ZemoNaNsky"); // kind of a cross between NaN, demon, and star wars
+        //System.out.println(b.compareTo(boo));
+        //System.out.println(boo.compareTo(b));
+        System.out.println(n.compareTo(nuuuu));
+        System.out.println(nuuuu.compareTo(n));
     }
 }
